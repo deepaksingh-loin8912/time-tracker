58d895e7295ca4d5c5c2866fe8f42a8d7e0d8eab
Deepak Singh <56249701+deepaksingh-loin8912@users.noreply.github.com>
2025-10-01 11:36:48 +0530

    Create push-docker-image.sh
    script

---
+++ scripts/push-docker-image.sh
@@ -0,0 +1,7 @@
+jq -c '.[]' "$JSON_FILE" | while read -r item; do
+  SOURCE=$(echo "$item" | jq -r '.source')
+  TAG=$(echo "$item" | jq -r '.tag')
+  TARGET_REPO=$(echo "$item" | jq -r '.target_repo')
+  ...
+  TAR_NAME="$(basename "$SOURCE")-${TAG}.tar"
+  TAR_PATH="docker_images/$TAR_NAME"
